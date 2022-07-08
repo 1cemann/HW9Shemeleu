a1d34e8919f3d54260b19f579e14bed1e2932b0c
Mad <ned_91@mail.ru>
2022-07-09 01:32:04 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет произведение 
+//натуральных чисел в промежутке от M до N.
+
+//M = 1; N = 5. ->120
+
+//M = 4; N = 6. -> 120
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите M: ");
+int M = Convert.ToInt32(ReadLine());
+WriteLine("Введите N: ");
+int N = Convert.ToInt32(ReadLine());
+Write(Multiple(M,N));
+
+double Multiple(double a, double b)
+{
+    return (a < b) ? a * Multiple(a + 1, b) : a;
+}
